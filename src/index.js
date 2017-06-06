f44b47a4b3c19e872238d479ebc51f54ad75a5ea
Seokjun Kim <colus001@me.com>
2017-06-06 20:12:21 +0900

    redirect http to https

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import App from 'modules/App'
 import 'milligram/dist/milligram.min.css'
 import 'styles/default.scss'
 
+if (process.env.NODE_ENV === 'production' && location.protocol === 'http:') {
+  location.href = 'https://colus001.github.io'
+}
+
 ReactDOM.render(
   <App />, document.getElementById('root'),
 )
